178e212f9fe3d7fc27b64d6367c45423b244d91c
Zahra Mdienh <madinehzahra76@gmail.com>
2022-10-05 21:32:51 +0330

    create about us page

--- src/pages/about-us/AboutUs.jsx
+++ src/pages/about-us/AboutUs.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import InputForm from "../../components/input-form/InputForm";
+import {aboutUs} from "../../data/aboutUs";
+import Button from "../../components/button/Button";
+import Container from "../../components/container/Container";
+import {Link} from "react-router-dom";
 
 const AboutUs = () => {
-  return <div>AboutUs</div>;
+    const inputChange = () => {
+
+    }
+
+  return (
+      <div className={'AboutUs'}>
+          <Container>
+
+              <div className={'Login__header'}>
+                  <Link to={'/'}>{" < برگشت به صفحه قبل"}</Link>
+              </div>
+              
+            <InputForm title={'لطفا مشخصات خود را جهت تحویل کالا تایید نمایید.'} inputs={aboutUs} inputChange={inputChange} />
+            <Button className={'AboutUs_btn'} primary={true} onClick={inputChange}>{'پرداخت'}</Button>
+          </Container>
+      </div>
+  )
 };
 
 export default AboutUs;
